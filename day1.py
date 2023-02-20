e80c5ddf54ae43b4343ddcaeda0e57c0780a1393
peterhirst1 <peterhirst1>
2023-02-20 14:21:06 +0000

    added some basic python

---
+++ day1.py
@@ -0,0 +1,36 @@
+# Databricks notebook source
+print("hello world")
+first_name = "peter"
+second_name = "hirst"
+
+bool1 =True
+bool2 = False
+
+int1 = 15
+int2 = 25
+int3 = -30
+
+float1 = 1.5
+float2 = 2.5
+
+list1 = [1,2,3,4,5]
+list_product = ["One", "Two" ," Three" ,"Four"]
+
+print(list_product)
+
+print("sum: ", number1 + number2)
+print("subtraction: ", number1 - number2)
+print("mult: ", number1 * number2)
+print("div: ", number1 + number2)
+
+print("Integer Division: ", number1 // number2)
+print("Modulo: ", number1 % number2)
+
+      
+
+# COMMAND ----------
+
+number1 = input("please enter a number")
+number2 = input("enter a second")
+
+print(int(number1) + int(number2))
